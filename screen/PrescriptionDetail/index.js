03c4de4304542b66a1c00dc6eb681301fa097721
homeswiz <homes@wizschool.io>
2021-09-24 19:55:59 +0900

    Add Prescription Detail Screen

---
+++ screen/PrescriptionDetail/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const PrescriptionDetail = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Prescription Detail</Text>
+    </View>
+  );
+};
+
+export default PrescriptionDetail;
