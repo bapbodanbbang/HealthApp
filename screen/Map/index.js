d027c14196e925cff1a803bc35fcdd690b58b2d1
homeswiz <homes@wizschool.io>
2021-09-26 15:37:09 +0900

    Update maps (need get Current My location)

--- screen/Map/index.js
+++ screen/Map/index.js
@@ -1,44 +1,62 @@
-import React, {useState} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 const Map = () => {
-  const [location, setLocation] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+  const [locations, setLocations] = useState([]);
+  let _watchId;
 
-  const [title, setTitle] = useState('title');
-  const [description, setDescription] = useState('discription');
+  useEffect(() => {
+    _watchId = Geolocation.watchPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocations([...locations, {latitude, longitude}]);
+      },
+      error => {
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 100,
+        interval: 5000,
+        fastestInterval: 2000,
+      },
+    );
+  }, [locations]);
+
+  useEffect(() => {
+    return () => {
+      if (_watchId !== null) {
+        Geolocation.clearWatch(_watchId);
+      }
+    };
+  }, []);
 
   return (
-    <MapView
-      style={{flex: 1}}
-      initialRegion={{
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: location.latitudeDelta,
-        longitudeDelta: location.longitudeDelta,
-      }}
-      onRegionChange={region => {
-        setLocation({
-          latitude: region.latitude,
-          longitude: region.longitude,
-        });
-      }}
-      onRegionChangeComplete={region => {
-        setLocation({
-          latitude: region.latitude,
-          longitude: region.longitude,
-        });
-      }}>
-      <Marker
-        coordinate={{latitude: 37.78825, longitude: -122.4324}}
-        title={title}
-        description={description}
-      />
-    </MapView>
+    <View style={{flex: 1}}>
+      {locations.length > 0 && (
+        <MapView
+          style={{flex: 1}}
+          initialRegion={{
+            latitude: locations[0].latitude,
+            longitude: locations[0].longitude,
+            latitudeDelta: 0.02,
+            longitudeDelta: 0.015,
+          }}>
+          {locations.map((location, index) => (
+            <Marker
+              key={`location-${index}`}
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+              }}
+            />
+          ))}
+        </MapView>
+      )}
+    </View>
   );
 };
 
