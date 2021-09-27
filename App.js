efb3df669bdcbfc9783323174f935c9c1733ff23
homeswiz <homes@wizschool.io>
2021-09-27 18:22:38 +0900

    Modify eslint

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import 'react-native-gesture-handler';
-import Home from "./screen/Home";
-import Prescriptions from "./screen/Prescriptions";
-import PrescriptionDetail from "./screen/PrescriptionDetail";
-import Map from "./screen/Map";
+import Home from './screen/Home';
+import Prescriptions from './screen/Prescriptions';
+import PrescriptionDetail from './screen/PrescriptionDetail';
+import Map from './screen/Map';
 
 // import 'react-native-gesture-handler';
 const Stack = createStackNavigator();
@@ -13,10 +13,13 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Map">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Prescriptions" component={Prescriptions} />
-        <Stack.Screen name="PrescriptionDetail" component={PrescriptionDetail} />
+        <Stack.Screen
+          name="PrescriptionDetail"
+          component={PrescriptionDetail}
+        />
         <Stack.Screen name="Map" component={Map} />
       </Stack.Navigator>
     </NavigationContainer>
